2bf088d7eae82e90b87c2eae24b5792234e84e7c
Rakibul12356 <mdrakibulhasan12346@gmail.com>
2024-10-16 19:20:22 +0600

    first commit

---
+++ src/components/Country.jsx
@@ -0,0 +1,14 @@
+
+import './Country.css'
+const Country = ({country}) => {
+    console.log(country)
+    const {name,flags}=country
+    return (
+        <div className='country'>
+            <h3>name:{name?.common}</h3>
+            <img src={flags.png} alt="" />
+        </div>
+    );
+};
+
+export default Country;
